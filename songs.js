e7d357095f4fb78b7d8ac854158575dd743ecc35
yoyou458 <yousefeltahawyashraf@gmail.com>
2026-03-28 13:53:01 -0700

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,8 @@
+const songs = [
+    {name:"حلف القمر", file:"songs/half_elamar.mp3", img:"images/half_elamar.jpg"},
+    {name:"الهوى سلطان", file:"songs/el_hawa_sultan.mp3", img:"images/el_hawa_sultan.jpg"},
+    {name:"ما تقوله ليه", file:"songs/ma_t2olou_leh.mp3", img:"images/ma_t2olou_leh.jpg"},
+    {name:"كلام الناس", file:"songs/kalam_el_nas.mp3", img:"images/kalam_el_nas.jpg"},
+    {name:"خسرت كل الناس", file:"songs/khesert_kol_el_nas.mp3", img:"images/khesert_kol_el_nas.jpg"},
+    {name:"اسمعيلي كلمة", file:"songs/isma3i_kelma.mp3", img:"images/isma3i_kelma.jpg"} // 
+];
